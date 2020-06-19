485323cd877522eeacbe281115190df6ad049340
Alessio <alessio2793@gmail.com>
2020-06-19 11:23:58 +0200

    Update make-slider.php

--- make-slider.php
+++ make-slider.php
@@ -11,17 +11,17 @@
  
  function tbare_wordpress_plugin_demo($atts) {
 	 
-	wp_register_script( 'slider-owl-js', plugins_url('slider/js/owl.carousel.min.js'), array( 'jquery' ) );
+	wp_register_script( 'slider-owl-js', plugin_dir_url( __FILE__ ).('/js/owl.carousel.min.js'), array( 'jquery' ) );
     wp_enqueue_script( 'slider-owl-js' );
 	
-	wp_register_script( 'slider-init-owl-js', plugins_url('slider/js/init.js'), array( 'jquery' ) );
+	wp_register_script( 'slider-init-owl-js', plugin_dir_url( __FILE__ ).('/js/init.js'), array( 'jquery' ) );
     wp_enqueue_script( 'slider-init-owl-js' );
 	
-	wp_enqueue_style( 'slider-owl-css', plugins_url( 'slider/css/owl.carousel.min.css' ) );
-	wp_enqueue_style( 'slider-css', plugins_url( 'slider/css/slider.css' ) );
+	wp_enqueue_style( 'slider-owl-css', plugin_dir_url( __FILE__ ).( '/css/owl.carousel.min.css' ) );
+	wp_enqueue_style( 'slider-css', plugin_dir_url( __FILE__ ).( '/css/slider.css' ) );
 	
-	wp_enqueue_style( 'slider-owl-theme-css', plugins_url( 'slider/css/owl.theme.default.min.css' ) );
-	wp_enqueue_style( 'slider-animate-css', plugins_url( 'slider/css/animate.css' ) );
+	wp_enqueue_style( 'slider-owl-theme-css', plugin_dir_url( __FILE__ ).( '/css/owl.theme.default.min.css' ) );
+	wp_enqueue_style( 'slider-animate-css', plugin_dir_url( __FILE__ ).( '/css/animate.css' ) );
 	
 	include 'template.php';
 	
@@ -29,4 +29,4 @@
 }
 
 add_shortcode('slider', 'tbare_wordpress_plugin_demo');
- 
+?>
